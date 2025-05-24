cb20c71dea361ddf078669928a96ec24a268327b
nghiahoangdever <hoangdainghia1818@gmail.com>
2025-05-24 16:29:35 +0700

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from './pages/Boards/_id'
+
+function App() {
+  return (
+    <>
+      {/* React Rounter Dom /boards /boards/{board_id} */}
+      {/* Board details */}
+      <Board />
+    </>
+  )
+}
+
+export default App
